e7649e64dfc43800bc8a4362679059b034991e01
tina bayat <tinaa.bayat@gmail.com>
2022-12-05 19:58:03 +0100

    the basic game is completed

--- Hangman.py
+++ Hangman.py
@@ -1,37 +1,108 @@
 import random
 
-HANGSMAN_PICS = ['''+---+
-                        |
-                        |
-                        |
-                      =====''' , 
-                '''+---+
-                   o   |
-                       |
-                       |
-                     =====''', 
-                '''+---+
-                   o   |
-                   |   |
-                       |                   
-                     =====''' , 
-                '''+---+
-                   o   |
-                   |\  |
-                       |
-                     =====''' , 
-                '''+---+
-                   o   |
-                  /|\  |
-                       |
-                     =====''' , 
-                '''+---+
-                   o   |
-                  /|\  |
-                    \  |
-                     =====''' , 
-                '''+---+
-                   o   |
-                  /|\  |
-                  / \  |
-                     =====''']
+HANGSMAN_PICS = ['''
+   +---+
+       |
+       |
+       |
+     =====''' , '''
+    +---+
+    o   |
+        |
+        |
+      =====''', '''
+    +---+
+    o   |
+    |   |
+        |                   
+      =====''' , '''
+    +---+
+    o   |
+    |\  |
+        |
+      =====''' , '''
+    +---+
+    o   |
+   /|\  |
+        |
+      =====''' , '''
+    +---+
+    o   |
+   /|\  |
+     \  |
+      =====''' , '''
+     +---+
+     o   |
+    /|\  |
+    / \  |
+       =====''']
+words = 'ant baboon badger bat bear beaver camel cat clam cobra cougar coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk lion lizard llama mole monkey moose mouse mule newt otter owl panda parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake spider stork swan tiger toad trout turkey turtle weasel whale wolf wombat zebra'.split()
+
+def getrandom_word(wordlist):
+    wordindex = random.randint(0 , len(wordlist) -1)
+    return wordlist[wordindex]
+
+def display_board(missedletter , correctletter, secretword):
+    print(f'{HANGSMAN_PICS[len(missedletter)]} \n')
+    print('missed letter:' , end=' ')
+    for letter in missedletter:
+        print(letter , end = ' ' )
+        print()
+
+    blanks= '_' * len(secretword)
+    for i in range(len(secretword)):
+        if secretword[i] in correctletter:
+            blanks = blanks[:i] + secretword[i] + blanks[i+1:]   
+    for letter in blanks:
+        print(letter , end = ' ')
+
+def getguess(alreadyguessed):
+    while True:
+        guess = input('\nplease guess a letter:').lower()
+        if len(guess) != 1:
+              print('\nPlease enter a single letter.')
+        elif guess in alreadyguessed:
+              print('\nYou have already guessed that letter. Choose again.')
+        elif guess not in 'abcdefghijklmnopqrstuvwxyz':
+              print('\nPlease enter a LETTER.')
+        else:
+             return guess
+
+def playagain():
+    answer = input('Do you want to play again? (yes or no)').lower().startswith('y')
+    return answer
+
+
+print('H A N G M A N')
+missedletter = ''
+correctletter = ''
+secretword = getrandom_word(words)
+gameisdone = False
+
+while True:
+    display_board(missedletter,correctletter,secretword)
+    guess = getguess(missedletter + correctletter)
+    if guess in secretword:
+        correctletter = correctletter + guess
+        foundall_letter = True
+        for i in range(len(secretword)):
+            if secretword[i] not in correctletter:
+                foundall_letter = False
+                break
+        if foundall_letter:
+            print("yes the secret word is " +secretword + " !you won!")
+            gameisdone = True
+    else:
+        missedletter = missedletter + guess
+        if len(missedletter) == len(HANGSMAN_PICS) -1 :
+            display_board(missedletter,correctletter,secretword)
+            print('You have run out of guesses!\nAfter ' + str(len(missedletter)) + ' missed guesses and ' + str(len(correctletter)) + ' correct guesses, the word was "' + secretword + '"')
+            gameisdone = True
+    if gameisdone:
+        if playagain():
+            missedletter = ''
+            correctletter = ''
+            secretword = getrandom_word(words)
+            gameisdone = False
+        else:
+            break
