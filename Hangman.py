89824ab268bf798cf73f09c0bdf1ef09fe6bbd9a
tina bayat <tinaa.bayat@gmail.com>
2022-12-07 15:23:26 +0100

    GUI features is added

--- Hangman.py
+++ Hangman.py
@@ -2,7 +2,7 @@ import random
 from PIL import Image
 import pyautogui as pg
 
-print('H A N G M A N')
+pg.alert('you start a dangrous game --> H A N G M A N ')
 
 im1=Image.open("/Users/tina/projects/coffee to do/Hangman_game/images/1.png") 
 im2= Image.open("/Users/tina/projects/coffee to do/Hangman_game/images/2.png")
@@ -28,16 +28,16 @@ game_is_done = False
 
 def display_board(missed_letter , correct_letter, secret_word):
     img_list[len(missed_letter)].show()
-    print('\nmissed letter:' , end=' ')
-    for letter in missed_letter:
-        print(f'\n{letter}' , end = ' \n' )
-    blanks= '_' * len(secret_word)
+    letter =''
+    letter += f'{missed_letter} '
+    pg.alert(f'missed letter : {letter} ')
+    blanks=  '_'  * len(secret_word)
     for i in range(len(secret_word)):
         if secret_word[i] in correct_letter:
             blanks = blanks[:i] + secret_word[i] + blanks[i+1:]   
-    for letter in blanks:
-        print(letter , end = ' ')
-
+    result= (" ".join(blanks))
+    pg.alert(f'secret word : {result}')
+    
 def get_guess(alreadyguessed):
     while True:
         guess = pg.prompt( 'please enter your guess' )
@@ -52,7 +52,8 @@ def get_guess(alreadyguessed):
              return guess
 
 def play_again():
-    answer = input('Do you want to play again? (yes or no)\n').lower().startswith('y')
+    answer = pg.prompt('Do you want to play again? (yes or no)\n')
+    answer= answer.lower().startswith('y')
     return answer
 
 def win(correct_letter):
@@ -62,14 +63,14 @@ def win(correct_letter):
             foundall_letter = False
             break
     if foundall_letter:
-        print("yes the secret word is " + secret_word + " !you won!")
+        pg.alert(f'yes the secret word is { secret_word } !\nyou won!')
         return True
     return False
 
 def loose(missed_letter):
     if len(missed_letter) == len(img_list) -1 :
         display_board(missed_letter,correct_letter,secret_word)
-        print('You have run out of guesses!\nAfter ' + str(len(missed_letter)) + ' missed guesses and ' + str(len(correct_letter)) + ' correct guesses, the word was "' + secret_word + '"')
+        pg.alert(f'You have run out of guesses!\nAfter {len(missed_letter)} missed guesses and {len(correct_letter)} correct guesses, the word was { secret_word }')
         return True
     return False
 
@@ -78,7 +79,6 @@ def loose(missed_letter):
 while True:
     display_board(missed_letter,correct_letter,secret_word)
     guess = get_guess(missed_letter + correct_letter)
-    img_list.close()
     if guess in secret_word:
         correct_letter = correct_letter + guess
         game_is_done = win(correct_letter)
